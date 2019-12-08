762b772d9ccc870e01f32e9cd331661f7949bb18
Manoj Singh <msinghb4u@gmail.com>
2019-12-08 13:23:30 +0530

    Add files via upload

---
+++ src/main/java/com/howtodoinjava/jersey/JerseyService.java
@@ -0,0 +1,15 @@
+package com.howtodoinjava.jersey;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/message")
+public class JerseyService
+{
+    @GET
+    public String getMsg()
+    {
+         return "Hello World !! - Jersey 2";
+    } 
+   
+}
